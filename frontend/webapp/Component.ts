0082c4f1551bc1bc89594cea94e9f191f5f9ba72
khavari <a_khavari94@yahoo.com>
2025-08-15 20:14:00 +0200

    create initial UIComponent setup for books-frontend

---
+++ frontend/webapp/Component.ts
@@ -0,0 +1,7 @@
+import UIComponent from "sap/ui/core/UIComponent";
+
+export default UIComponent.extend("books-frontend.Component", {
+  metadata: {
+    manifest: "json"
+  }
+}) as unknown as UIComponent;
